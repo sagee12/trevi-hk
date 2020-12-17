4125c41875d05556d73ad178258d83e87568bf5b
sagar <sagargurung500@yahoo.com>
2020-12-17 17:52:49 +0800

    some changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('admin') }}">
-                   Trevy Worker's Group
+                   Trevy Admin
                 </a>
                 <a class="navbar-brand" href="{{route('blog')}}">Trevy site</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
